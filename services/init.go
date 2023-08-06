9e62ca155a5ee9cfe4df7a4e565a62cfbd0c581d
Dan Chen <dan.chen@boardware.com>
2023-08-06 12:40:00 +0800

    fixup

--- services/init.go
+++ services/init.go
@@ -1,8 +1,11 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/Dparty/model"
 	"github.com/Dparty/model/core"
+	"github.com/spf13/viper"
 
 	"gorm.io/gorm"
 )
@@ -10,22 +13,22 @@ import (
 var DB *gorm.DB
 
 func init() {
-	// var err error
-	// viper.SetConfigName(".env.yaml")
-	// viper.SetConfigType("yaml")
-	// viper.AddConfigPath(".")   // optionally look for config in the working directory
-	// err = viper.ReadInConfig() // Find and read the config file
-	// if err != nil {            // Handle errors reading the config file
-	// 	panic(fmt.Errorf("databases fatal error config file: %w", err))
-	// }
-	user := "warmsilver"
-	password := "warmsilver"
-	host := "localhost"
-	port := "3306"
-	database := "warmsilver"
-	DB, _ = model.NewConnection(user, password, host, port, database)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	var err error
+	viper.SetConfigName(".env.yaml")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")   // optionally look for config in the working directory
+	err = viper.ReadInConfig() // Find and read the config file
+	if err != nil {            // Handle errors reading the config file
+		panic(fmt.Errorf("databases fatal error config file: %w", err))
+	}
+	user := viper.GetString("database.user")
+	password := viper.GetString("database.password")
+	host := viper.GetString("database.host")
+	port := viper.GetString("database.port")
+	database := viper.GetString("database.database")
+	DB, err = model.NewConnection(user, password, host, port, database)
+	if err != nil {
+		panic(err)
+	}
 	DB.AutoMigrate(&core.Account{})
 }
