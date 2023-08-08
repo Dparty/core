874a7a9db1aee66aff8bb4a2a4b21871969af263
Dan Chen <dan.chen@boardware.com>
2023-08-08 11:46:38 +0800

    fixup

--- controllers/init.go
+++ controllers/init.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/Dparty/common/server"
 	api "github.com/Dparty/core-api"
 	"github.com/gin-gonic/gin"
@@ -11,6 +13,9 @@ var router *gin.Engine
 func Init() {
 	router = gin.Default()
 	router.Use(server.CorsMiddleware())
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{})
+	})
 	api.AccountApiInterfaceMounter(router, accountApi)
 	api.RestaurantApiInterfaceMounter(router, restaurantApi)
 }
