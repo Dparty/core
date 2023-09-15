abd1b81963612119379b45cb6a19b77048a98779
Dan Chen <dan.chen@boardware.com>
2023-09-15 17:07:19 +0800

    feat

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Dparty/core/controllers"
-	"github.com/Dparty/core/services"
 )
 
 func main() {
-	// var err error
-	err := services.UpdatePasswordForce(1681137588590612482, "hello")
-	fmt.Println(err)
-	// viper.SetConfigName(".env") // name of config file (without extension)
-	// viper.SetConfigType("yaml")
-	// viper.AddConfigPath(".")   // optionally look for config in the working directory
-	// err = viper.ReadInConfig() // Find and read the config file
-	// if err != nil {            // Handle errors reading the config file
-	// 	panic(fmt.Errorf("databases fatal error config file: %w", err))
-	// }
-	// port := ":" + viper.GetString("server.port")
 	controllers.Init()
 	controllers.Run(":8080")
 }
