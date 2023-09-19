2b147b117f981b9ddf8cb28249eef2b2bb7201e4
Dan Chen <dan.chen@boardware.com>
2023-09-19 17:35:20 +0800

    feat

--- controllers/init.go
+++ controllers/init.go
@@ -20,9 +20,10 @@ func Init() {
 		ctx.JSON(http.StatusOK, gin.H{})
 	})
 	router.POST("/feieyun", func(ctx *gin.Context) {
-
+		// orderId := ctx
 	})
 	router.GET("/feieyun/feieyun_verify_3E6TRJ5g81bCsdZI.txt", func(ctx *gin.Context) {
+		ctx.Header("Content-Type", "text/plain")
 		ctx.String(http.StatusOK, "3E6TRJ5g81bCsdZI")
 	})
 	api.AccountApiInterfaceMounter(router, accountApi)
