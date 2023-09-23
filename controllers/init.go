06808315210793a55a7bf7d9dc9f10daa517ecf8
Dan Chen <dan.chen@boardware.com>
2023-09-23 10:14:38 +0800

    feat

--- controllers/init.go
+++ controllers/init.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Dparty/common/server"
@@ -9,6 +10,13 @@ import (
 )
 
 var router *gin.Engine
+
+type FeieyunCallback struct {
+	OrderId string `form:"orderId" json:"orderId" binding:"required"`
+	Status  int    `form:"status" json:"status" binding:"required"`
+	Stime   int    `form:"stime" json:"stime" binding:"required"`
+	Sign    string `form:"sign" json:"sign" binding:"required"`
+}
 
 func Init() {
 	router = gin.Default()
@@ -19,8 +27,14 @@ func Init() {
 	router.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{})
 	})
-	router.POST("/feieyun", func(ctx *gin.Context) {
-		// orderId := ctx
+	router.POST("/feieyun/callback", func(ctx *gin.Context) {
+		var feieyunCallback FeieyunCallback
+		if err := ctx.ShouldBind(&feieyunCallback); err != nil {
+			ctx.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		fmt.Println(feieyunCallback)
+		ctx.JSON(200, gin.H{"data": "info.A "})
 	})
 	router.GET("/feieyun/feieyun_verify_3E6TRJ5g81bCsdZI.txt", func(ctx *gin.Context) {
 		ctx.Header("Content-Type", "text/plain")
